cb4c4602949ce801eb8dbbff81edfc38b44697d9
Alexia <94938165+AlexiaNi@users.noreply.github.com>
2023-07-15 17:12:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(
+   name='BSafe',
+   version='1',
+   description='BSafe Password Manager',
+   author='Niculae Alexia',
+   packages=['BSafe'],  #same as name
+   install_requires=['tkinter', 'sqlite3', 'PIL', 'os'], 
+)
